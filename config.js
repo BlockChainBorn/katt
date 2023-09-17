e5502228e643443ffaed772c03acc36e19c8c7c4
PiratePennies <103362448+BlockChainBorn@users.noreply.github.com>
2023-09-17 11:59:31 +0200

    Add files via upload

--- config.js
+++ config.js
@@ -1,27 +1,27 @@
 
 // Server configuration
-export const SERVER_PORT = 3000; // Server port
-export const DEBUG = false; // Debug mode
+const SERVER_PORT = 3000; // Server port
+const DEBUG = false; // Debug mode
 
 // Prompt Moderation before sending to OpenAI
-export const MODERATION = true; // Moderation mode
+const MODERATION = true; // Moderation mode
 
 // Rate limit
-export const PRIOD = 15 * 1000; // 15 seconds
-export const RATE_LIMIT = 50; // 50 requests per 15 seconds
+const PRIOD = 15 * 1000; // 15 seconds
+const RATE_LIMIT = 50; // 50 requests per 15 seconds
 
 // Whitelisted IPs
-export const WHITELISTED_IPS = [
+const WHITELISTED_IPS = [
     // "127.0.0.1"
 ];
 
 // OpenAI API Keys
-export let OPENAI_KEYS = [
+let OPENAI_KEYS = [
     "pk-sDltwisEgCTSpyyUHlQQtpvIETpiwdapnoFwhabZ",
 ];
 
 // Import OpenAI modules
-import { Configuration, OpenAIApi } from "openai";
+const { Configuration, OpenAIApi } = require("openai");
 
 // Initialize API configuration
 const configuration = new Configuration({
@@ -32,16 +32,20 @@ const configuration = new Configuration({
 // Create OpenAI API instance
 const openai = new OpenAIApi(configuration);
 
-// Example OpenAI API call
-const response = await openai.createCompletion({
-    model: "text-davinci-003",
-    prompt: "Human: Hello\nAI:",
-    temperature: 0.7,
-    max_tokens: 256,
-    top_p: 1,
-    frequency_penalty: 0,
-    presence_penalty: 0,
-    stop: ["Human: ", "AI: "],
-});
+async function exampleCall() {
+    // Example OpenAI API call
+    const response = await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt: "Human: Hello\nAI:",
+        temperature: 0.7,
+        max_tokens: 256,
+        top_p: 1,
+        frequency_penalty: 0,
+        presence_penalty: 0,
+        stop: ["Human: ", "AI: "],
+    });
 
-console.log(response.data.choices[0].text);
+    console.log(response.data.choices[0].text);
+}
+
+exampleCall();
